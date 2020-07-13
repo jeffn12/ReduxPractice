2dcaf71051672dc247b58e6be44f6781e87e28f3
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-13 08:57:47 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ *  createStore function
+ * @description a function to mimic a redux store
+ */
+
+function createStore() {
+  /**
+   *  Store should have 4 parts:
+   *   1. the state
+   *   2. get the state
+   *   3. listen to changes on state
+   *   4. update the state
+   */
+
+  let state;
+
+  const getState = () => state;
+}
