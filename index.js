fd8e61e887a9263b8847dd7235c76dd169d74a6d
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-13 12:25:30 -0400

    fix: id gen return reusable string

--- index.js
+++ index.js
@@ -44,9 +44,9 @@ function createStore(reducer) {
  *  ID Generator helper function
  */
 const generateID = function () {
-  return (
-    Math.random().toString(36).substring(2) * new Date().getTime().toString(36)
-  );
+  const id = Math.random().toString(36).substring(2); // * new Date().getTime().toString(36)
+  console.log(id);
+  return id;
 };
 
 /**
